20db424700d99a8ead8cdbe571c65d0aa445353a
Libin Song <lisong@microsoft.com>
2023-08-13 22:54:05 -0700

    remove unused files

--- vite-ext/src/background.ts
+++ vite-ext/src/background.ts
@@ -1,11 +1,17 @@
-import browser from "webextension-polyfill";
-
 console.log("Hello from the background!");
 
 chrome.runtime.onInstalled.addListener((details) => {
   console.log("Extension installed:", details);
+
+  let title = "Add to my vocabulary";
+  chrome.contextMenus.create({
+    title: title,
+    contexts: ["selection"],
+    id: "selection"
+  });
 });
 
 chrome.sidePanel
   .setPanelBehavior({ openPanelOnActionClick: true })
   .catch((error) => console.error(error));
+
