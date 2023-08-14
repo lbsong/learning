ba631b075d9abfc90c7984596f5272c882f2aaa2
Libin Song <lisong@microsoft.com>
2023-08-13 23:21:35 -0700

    add back context menu

--- vite-ext/src/background.ts
+++ vite-ext/src/background.ts
@@ -1,4 +1,34 @@
+
 console.log("Hello from the background!");
+
+// A generic onclick callback function.
+chrome.contextMenus.onClicked.addListener((info: chrome.contextMenus.OnClickData) => {
+    if (info.menuItemId === "selection") {
+        genericOnClick(info, null);
+    }
+});
+
+// A generic onclick callback function.
+async function genericOnClick(info: any, tab: any) {
+    let word = info.selectionText;
+
+    let setResult = await chrome.storage.sync.get([word]);
+    let frequency = 1;
+    if (Object.keys(setResult).length != 0) {
+        frequency = setResult[word].frequency + 1;
+    }
+
+    console.log(frequency);
+    var vocab = { "frequency": frequency }
+    await chrome.storage.sync.set({[word]: vocab});
+    let getResult = await chrome.storage.sync.get([word]);
+    console.log(getResult);
+
+    chrome.runtime.sendMessage({
+        name: 'define-word',
+        data: { value: word }
+      });
+}
 
 chrome.runtime.onInstalled.addListener((details) => {
   console.log("Extension installed:", details);
@@ -15,3 +45,4 @@ chrome.sidePanel
   .setPanelBehavior({ openPanelOnActionClick: true })
   .catch((error) => console.error(error));
 
+export {};
