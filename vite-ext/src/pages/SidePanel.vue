45e2726068f4fb344d2c029a981650ec052944ae
Libin Song <lisong@microsoft.com>
2023-09-02 11:59:38 -0700

    try to figure out how to iterate list in vue

--- vite-ext/src/pages/SidePanel.vue
+++ vite-ext/src/pages/SidePanel.vue
@@ -1,35 +1,66 @@
 <template>
     <h1>SidePanel Test</h1>
-    <div id="divWord"> New word is: {{ word }}</div>
+    <div id="divWord">
+        <h2>{{ word.value }}</h2>
+        <ul>
+            <li v-for="def in response.value">
+                <h3>{{ def.phonetic }}</h3>
+                <ul>
+                    <li v-for="meaning in def.meanings">
+                        <h4>{{ meaning.partOfSpeech }}</h4>
+                        <ul>
+                            <li v-for="definition in meaning.definitions">
+                                <p>{{ definition.definition }}</p>
+                                <p>{{ definition.example }}</p>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script lang="ts" setup>
-    import { ref } from "vue";
-    console.log("Hello from the side panel!");
-    // import OpenAI from "openai";
+    import { reactive } from 'vue'
+    const word = reactive({ value: '' });
 
-    // const openai = new OpenAI(process.env.VUE_APP_OPENAI_API_KEY);
-    // const completion = await openai.chat.completions.create({
-    //     messages: [
-    //         {
-    //             role: "user",
-    //             content: "Hello, how are you?",
-    //         }
-    //     ],
-    //     model: "davinci"
-    // });
+    interface Phonetic {
+        text: string;
+        audio: string;
+        sourceUrl: string;
+    }
 
-    // console.log(completion.choices);
+    interface Meaning {
+        partOfSpeech: string;
+        definitions: Array<Definition>;
+    }
 
-    let word = ref("test");
+    interface Definition {
+        definition: string;
+        example: string;
+    }
 
-    chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    interface DefinitionResponse {
+        word: string;
+        phonetic: string;
+        phonetics: Array<Phonetic>;
+        meanings: Array<Meaning>;
+    }
+
+    const response = reactive({ value: Array<DefinitionResponse> });
+
+    chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
         console.log("Message received!");
         console.log(message);
         console.log(sender);
-        console.log(sendResponse)
+        console.log(sendResponse);
+        word.value = message.data.value;
 
-        word = ref(message.data.value);
-        console.log("Word is: " + word);
-    });
+        const def = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word.value}`);
+        const defJson = await def.json();
+        console.log(defJson);
+
+        response.value = defJson;
+    })
 </script>
